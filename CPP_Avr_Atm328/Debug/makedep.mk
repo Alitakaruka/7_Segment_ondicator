6f511af318f188597c717601cf46586b7f603857
Alitakaruka <galashevich68@gmail.com>
2023-08-02 09:12:39 +0300

    End

--- CPP_Avr_Atm328/Debug/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-7Segment\seven-segment indicator.c
-
-main.c
-
